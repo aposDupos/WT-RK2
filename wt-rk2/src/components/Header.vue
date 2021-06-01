5bf1358784d325401b0440da8c66074715193ff0
Vlad Tyrtychnyi <tva100702@gmail.com>
2021-06-01 13:25:18 +0300

    Added Header.vue

--- wt-rk2/src/components/Header.vue
+++ wt-rk2/src/components/Header.vue
@@ -1,8 +1,19 @@
 <template>
-  <header class="header">
-
-
-  </header>
+    <header>
+      <div class="container">
+        <div class="header">
+        <div class="header-logo"><img  alt="Логотип"
+                                      srcset="../assets/imgs/logo-32.png 1x, ../assets/imgs/logo-64.png 2x">
+        </div>
+        <nav class="header-nav">
+          <div class="header-nav__item"><a href="#CSS-frameworks">CSS фреймворки</a></div>
+          <div class="header-nav__item"><a href="#CSS-preproccessors">CSS препроцессоры</a></div>
+          <div class="header-nav__item"><a href="#JS-frameworks">Фреймворки JavaScript</a></div>
+        </nav>
+        <button class="header-theme-switcher">Сменить тему</button>
+      </div>
+      </div>
+    </header>
 </template>
 
 <script>
--- wt-rk2/src/views/Main.vue
+++ wt-rk2/src/views/Main.vue
@@ -1,12 +1,14 @@
 <template>
 <div class="main">
-
+<Header/>
 </div>
 </template>
 
 <script>
+import Header from "../components/Header";
 export default {
-  name: "Main"
+  name: "Main",
+  components: {Header},
 }
 </script>
 
